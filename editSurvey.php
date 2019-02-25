3430b4c62c619363274b0e4e711c42f64d5d5285
Marcus Brohede <a97marbr@gmail.com>
2019-02-25 10:34:08 +0100

    Edit survey fields

--- editSurvey.php
+++ editSurvey.php
@@ -172,14 +172,32 @@ session_start();
 							$debug = "Error updating database: " . $error[2];
 					}					
 			}else if($cmd=="UPD"){
-					echo "UPDATING".$id;
+          echo "UPDATING".$id;
+          $updateArr=array();
 					if($type!="UNK"){
-							echo "TYPE";
+              echo "TYPE";
+              array_push($updateArr,array("column"=>"type","value"=>$type));
 					}else if($labelA!="UNK"){
-							echo "LABL";					
-					}else if($desc!="UNK"){
-							echo "DESC";					
-					}
+              echo "LABL";	
+              array_push($updateArr,array("column"=>"labelA","value"=>$labelA));
+              array_push($updateArr,array("column"=>"labelB","value"=>$labelB));
+              array_push($updateArr,array("column"=>"labelC","value"=>$labelC));
+					}else if($description!="UNK"){
+              echo "DESC";					
+              array_push($updateArr,array("column"=>"description","value"=>$description));
+          }
+
+          foreach($updateArr as $update){
+              $sql='UPDATE item SET '.$update["column"].'=:value WHERE id=:id;';
+              $query = $log_db->prepare($sql);					
+              $query->bindParam(':id', $id);
+              $query->bindParam(':value', $update["value"]);
+              if (!$query->execute()) {
+                  $error = $query->errorInfo();
+                  $debug = "Error updating database: " . $error[2];
+              }					  
+          }
+          
 /*
 					$query = $log_db->prepare('DELETE FROM item WHERE id=:id;');					
 					$query->bindParam(':id', $id);				
